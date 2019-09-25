4854e396472067a18923f5570319fa22ea030014
SarthakJShetty <sarthakshetty97@gmail.com>
2019-09-25 10:32:04 +0530

    Adding the variables file here for ease of accessing variables across different files.

---
+++ quad/include/quad/variables/variables.hpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+//These vectors hold the X and the Y
+vector<int> maximum_value_x_indices;
+vector<int> maximum_value_y_indices;
