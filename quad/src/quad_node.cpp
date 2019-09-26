34da45f1e918449fe166b3dc7e27d61fbbf6a584
SarthakJShetty <sarthakshetty97@gmail.com>
2019-09-26 12:24:47 +0530

    Still working on this bit. Quad not relocating to given waypoint when distance <0.5

--- quad/src/quad_node.cpp
+++ quad/src/quad_node.cpp
@@ -9,51 +9,67 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <math.h>
 #include "weight.hpp"
 #include "variables.hpp"
 
-
 mavros_msgs::State current_state;
+//current_position is used to check whether or not the drone can take the next set of waypoints or not
+float current_position_x;
+float current_position_y;
+float current_position_z;
 
-// extern vector<int> maximum_value_x_indices;
-// extern vector<int> maximum_value_y_indices;
+//This keeps track of the waypoint currently conveyed to the UAV. If = 100 stops
+int counter = 0;
 
+//Distance keeps track of initial position and desired positon
+float dist;
 
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
+void state_cb(const mavros_msgs::State::ConstPtr &msg)
+{
     current_state = *msg;
+}
+
+void pose_sub(const geometry_msgs::PoseStamped msg)
+{
+    current_position_x = msg.pose.position.x;
+    current_position_y = msg.pose.position.y;
+    current_position_z = msg.pose.position.z;
 }
 
 int main(int argc, char **argv)
 {
+    //Generates the waypoints for the UAV to followw
     weight_generator_function(uav_x_position, uav_y_position, survivor_direction, x_corner_coordinate_1, x_corner_coordinate_2, x_corner_coordinate_3, x_corner_coordinate_4, y_corner_coordinate_1, y_corner_coordinate_2, y_corner_coordinate_3, y_corner_coordinate_4, maximum_value, maximum_value_x_indices, maximum_value_y_indices, map_priority, element_cycler);
+
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("mavros/setpoint_position/local", 10);
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
+    ros::Subscriber position_subscriber = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 10, pose_sub);
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
+    geometry_msgs::PoseStamped pose;
+
     // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
+    while (ros::ok() && !current_state.connected)
+    {
         ros::spinOnce();
         rate.sleep();
     }
 
-    geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 10;
-    pose.pose.position.y = 10;
-    pose.pose.position.z = 2;
-
     //send a few setpoints before starting
-    for(int i = 100; ros::ok() && i > 0; --i){
+    for (int i = 100; ros::ok() && i > 0; --i)
+    {
         local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
@@ -67,19 +83,62 @@ int main(int argc, char **argv)
 
     ros::Time last_request = ros::Time::now();
 
-    while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
+    while (ros::ok())
+    {
+
+        if (counter == 0)
+        {
+            pose.pose.position.x = 10;
+            pose.pose.position.y = 10;
+            pose.pose.position.z = 2;
+        }
+
+        dist = sqrt(pow((pose.pose.position.x - current_position_x), 2) + pow((pose.pose.position.y - current_position_y), 2));
+
+        cout << "Current X Position" << current_position_x << endl;
+        cout << "Current Y Position" << current_position_y << endl;
+        cout << "Current X Waypoint" << pose.pose.position.x << endl;
+        cout << "Current Y Waypoint" << pose.pose.position.y << endl;
+        
+        cout << "Distance" << dist << endl;
+
+        if (dist < 0.5)
+        {
+            cout << "Distance <0.5" << endl;
+            if (counter < 100)
+            {
+                cout << "Counter" << counter < endl;
+                counter += 1;
+                pose.pose.position.x = maximum_value_x_indices[counter];
+                pose.pose.position.y = maximum_value_y_indices[counter];
+                pose.pose.position.z = 5;
+            }
+            else
+            {
+                cout << "RTL" << endl;
+                pose.pose.position.x = 10;
+                pose.pose.position.y = 10;
+                pose.pose.position.z = 2;
+            }
+        }
+        if (current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0)))
+        {
+            if (set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent)
+            {
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
-        } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
+        }
+        else
+        {
+            if (!current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0)))
+            {
+                if (arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success)
+                {
                     ROS_INFO("Vehicle armed");
                 }
                 last_request = ros::Time::now();
