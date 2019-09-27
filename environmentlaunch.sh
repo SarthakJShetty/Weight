fcc41984b419c47c47cfe8ba678fc1114b49c722
SarthakJShetty <sarthakshetty97@gmail.com>
2019-09-27 12:32:37 +0530

    Multi UAV launch file added here as well, for prototyping with swarms.

--- environmentlaunch.sh
+++ environmentlaunch.sh
@@ -3,4 +3,4 @@ source ~/catkin_ws/devel/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
-roslaunch px4 posix_sitl.launch
+roslaunch px4 multi_uav_mavros_sitl.launch
