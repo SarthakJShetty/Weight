fc20d84b16c37e99f451ba821f7584b8fc1051ff
SarthakJShetty <sarthakshetty97@gmail.com>
2019-09-28 18:02:25 +0530

    Still trying to debug this bit, publishers are not being initialized

--- quad/src/quad_node.cpp
+++ quad/src/quad_node.cpp
@@ -13,19 +13,19 @@
 #include "weight.hpp"
 #include "variables.hpp"
 
-mavros_msgs::State current_state;
+mavros_msgs::State current_state[N_UAV];
 
 void state_cb(const mavros_msgs::State::ConstPtr &msg)
 {
-    current_state = *msg;
+    current_state[pre_pub_sub_initializer] = *msg;
 }
 
 void pose_sub(const geometry_msgs::PoseStamped msg)
 {
     //Subscribing to the current position of the UAV
-    current_position_x = msg.pose.position.x;
-    current_position_y = msg.pose.position.y;
-    current_position_z = msg.pose.position.z;
+    current_position_x[pre_pub_sub_initializer] = msg.pose.position.x;
+    current_position_y[pre_pub_sub_initializer] = msg.pose.position.y;
+    current_position_z[pre_pub_sub_initializer] = msg.pose.position.z;
 }
 
 int main(int argc, char **argv)
@@ -38,120 +38,209 @@ int main(int argc, char **argv)
 
     ros::NodeHandle nh;
 
-    //Subscribes to the current position of the UAV
-    ros::Subscriber position_subscriber = nh.subscribe<geometry_msgs::PoseStamped>("uav0/mavros/local_position/pose", 10, pose_sub);
+    /*
+    What are we trying to incorporate here?
+    1. A for loop from 0 -> N_UAV
+    2. Get the string that goes into the subscriber/publisher
+    3. Declare N_UAVs for each subscriber/publisher
+    4. Store each UAVs information in the corresponding publisher/subscriber
+    */
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("uav0/mavros/state", 10, state_cb);
-    //Topic to publish to, to change the UAVs position
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("uav0/mavros/setpoint_position/local", 10);
-    //Topic to check whether the UAV is armed or not
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("uav0/mavros/cmd/arming");
-    //Topic to check whether control is ONBOARD or OFFBOARD
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("uav0/mavros/set_mode");
-    
+    ros::Subscriber position_subscriber[N_UAV];
+    ros::Subscriber state_sub[N_UAV];
+    ros::Publisher local_pos_pub[N_UAV];
+    ros::ServiceClient arming_client[N_UAV];
+    ros::ServiceClient set_mode_client[N_UAV];
+
+    for (pre_pub_sub_initializer = 0; pre_pub_sub_initializer < N_UAV; pre_pub_sub_initializer++)
+    {
+        stringstream pub_sub_initializer;
+        pub_sub_initializer << pre_pub_sub_initializer;
+
+        string position_subscriber_string;
+        position_subscriber_string = "/uav" + pub_sub_initializer.str() + "/mavros/local_position/pose";
+        cout << "position_subscriber_string: " << position_subscriber_string << endl;
+        position_subscriber[pre_pub_sub_initializer] = nh.subscribe<geometry_msgs::PoseStamped>(position_subscriber_string, 10, pose_sub);
+
+        string state_sub_string;
+        state_sub_string = "/uav" + pub_sub_initializer.str() + "/mavros/state";
+        cout << "state_sub_string: " << state_sub_string << endl;
+        state_sub[pre_pub_sub_initializer] = nh.subscribe<mavros_msgs::State>(state_sub_string, 10, state_cb);
+
+        string local_pos_pub_string;
+        local_pos_pub_string = "/uav" + pub_sub_initializer.str() + "/mavros/setpoint_position/local";
+        cout << "local_pos_pub_string: " << local_pos_pub_string << endl;
+        local_pos_pub[pre_pub_sub_initializer] = nh.advertise<geometry_msgs::PoseStamped>(local_pos_pub_string, 10);
+
+        string arming_client_string;
+        arming_client_string = "/uav" + pub_sub_initializer.str() + "/mavros/cmd/arming";
+        cout << "arming_client_string: " << arming_client_string << endl;
+        arming_client[pre_pub_sub_initializer] = nh.serviceClient<mavros_msgs::CommandBool>(arming_client_string);
+
+        string set_mode_client_string;
+        set_mode_client_string = "/uav" + pub_sub_initializer.str() + "/mavros/set_mode";
+        cout << "set_mode_client_string: " << set_mode_client_string << endl;
+        set_mode_client[pre_pub_sub_initializer] = nh.serviceClient<mavros_msgs::SetMode>(set_mode_client_string);
+
+        cout << endl;
+    }
+
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
-    geometry_msgs::PoseStamped pose;
+    geometry_msgs::PoseStamped pose[N_UAV];
+
+    for (int UAV_COUNTER = 0; UAV_COUNTER < N_UAV; UAV_COUNTER++)
+    {
+        cout << "PUSHING TO POSE OF UAV: " << UAV_COUNTER << endl;
+        pose[UAV_COUNTER].pose.position.x = 0;
+        pose[UAV_COUNTER].pose.position.y = 0;
+        pose[UAV_COUNTER].pose.position.z = 2;
+    }
+
+    for (int UAV_COUNTER = 0; UAV_COUNTER < N_UAV; UAV_COUNTER++)
+    {
+        cout << "PRESENTING POSE OF UAV: " << UAV_COUNTER << endl;
+        cout << "X: " << pose[UAV_COUNTER].pose.position.x << endl;
+        cout << "Y: " << pose[UAV_COUNTER].pose.position.y << endl;
+        cout << "Z: " << pose[UAV_COUNTER].pose.position.z << endl;
+    }
+
+    int connected_state_counter;
+
+    for (int i = 0; i < N_UAV; i++)
+    {
+        if (!current_state[i].connected)
+        {
+            cout << "CURRENT_STATE" << endl;
+            connected_state_counter += 1;
+        }
+    }
 
     // wait for FCU connection
-    while (ros::ok() && !current_state.connected)
+    while (ros::ok() && (connected_state_counter == N_UAV))
     {
+        cout << "CURRENT_STATE FCU NOT TRIGGERED" << endl;
         ros::spinOnce();
         rate.sleep();
     }
 
     //send a few setpoints before starting
-    for (int i = 100; ros::ok() && i > 0; --i)
+    for (int UAV_COUNTER = 0; UAV_COUNTER < N_UAV; UAV_COUNTER++)
     {
-        local_pos_pub.publish(pose);
-        ros::spinOnce();
-        rate.sleep();
+        for (int i = 100; ros::ok() && i > 0; --i)
+        {
+            local_pos_pub[UAV_COUNTER].publish(pose[UAV_COUNTER]);
+            ros::spinOnce();
+            rate.sleep();
+        }
+    }
+    mavros_msgs::SetMode offb_set_mode[N_UAV];
+
+    for (int i = 0; i < N_UAV; i++)
+    {
+        cout << "OFFBOARD TRIGGER" << endl;
+        offb_set_mode[i].request.custom_mode = "OFFBOARD";
     }
 
-    mavros_msgs::SetMode offb_set_mode;
-    offb_set_mode.request.custom_mode = "OFFBOARD";
+    mavros_msgs::CommandBool arm_cmd[N_UAV];
+    for (int i = 0; i < N_UAV; i++)
+    {
+        cout << "ARMING" << endl;
+        arm_cmd[i].request.value = true;
+    }
 
-    mavros_msgs::CommandBool arm_cmd;
-    arm_cmd.request.value = true;
-
-    ros::Time last_request = ros::Time::now();
+    ros::Time last_request[N_UAV];
+    for (int UAV_COUNTER = 0; UAV_COUNTER < N_UAV; UAV_COUNTER++)
+    {
+        cout << "LAST REQUEST" << endl;
+        last_request[UAV_COUNTER] = ros::Time::now();
+    }
 
     while (ros::ok())
     {
-        if (counter == -1)
+        for (int UAV_COUNTER = 0; UAV_COUNTER < N_UAV; UAV_COUNTER++)
         {
-            counter += 1;
-            //Initial set of coordinates being published to the position topic of the UAV
-            pose.pose.position.x = list_maximum_value_y_indices[counter];
-            pose.pose.position.y = list_maximum_value_x_indices[counter];
-            pose.pose.position.z = 2;
-        }
-        else
-        {
-            //Difference between the current position and the next waypoint (x, y)
-            dist = sqrt(pow((pose.pose.position.x - current_position_x), 2) + pow((pose.pose.position.y - current_position_y), 2));
-
-            //From hereon out, we switch the X and Y coordinates to match that of the map
-
-            //Printing the current position of the UAV
-            cout << "Current X Position: " << current_position_y << endl;
-            cout << "Current Y Position: " << current_position_x << endl;
-
-            //Printing the waypoint that the UAV has to reach
-            cout << "Current X Waypoint: " << pose.pose.position.y << endl;
-            cout << "Current Y Waypoint: " << pose.pose.position.x << endl;
-
-            if (dist < 0.5)
+            if (counter[UAV_COUNTER] == -1)
             {
-                cout << "Distance < 0.5" << endl;
-                if (counter < (y_max * x_max))
+                counter[UAV_COUNTER] += 1;
+                //Initial set of coordinates being published to the position topic of the UAV
+                pose[UAV_COUNTER].pose.position.x = list_maximum_value_y_indices[counter[UAV_COUNTER]];
+                pose[UAV_COUNTER].pose.position.y = list_maximum_value_x_indices[counter[UAV_COUNTER]];
+                pose[UAV_COUNTER].pose.position.z = 2;
+            }
+            else
+            {
+                //Difference between the current position and the next waypoint (x, y)
+                dist = sqrt(pow((pose[UAV_COUNTER].pose.position.x - current_position_x[UAV_COUNTER]), 2) + pow((pose[UAV_COUNTER].pose.position.y - current_position_y[UAV_COUNTER]), 2));
+
+                //From hereon out, we switch the X and Y coordinates to match that of the map
+
+                //Printing the current position of the UAV
+                cout << "UAV_COUNTER: " << UAV_COUNTER << " "
+                     << "Current X Position: " << current_position_y[UAV_COUNTER] << endl;
+                cout << "UAV_COUNTER: " << UAV_COUNTER << " "
+                     << "Current Y Position: " << current_position_x[UAV_COUNTER] << endl;
+
+                //Printing the waypoint that the UAV has to reach
+                cout << "UAV_COUNTER: " << UAV_COUNTER << " "
+                     << "Current X Waypoint: " << pose[UAV_COUNTER].pose.position.y << endl;
+                cout << "UAV_COUNTER: " << UAV_COUNTER << " "
+                     << "Current Y Waypoint: " << pose[UAV_COUNTER].pose.position.x << endl;
+
+                if (dist < 0.5)
                 {
-                    cout << "Counter" << counter << endl;
-                    cout << "Maximum_Value_X_Indices: " << counter << " " << list_maximum_value_x_indices[counter] << endl;
-                    cout << "Maximum_Value_Y_Indices: " << counter << " " << list_maximum_value_y_indices[counter] << endl;
-                    pose.pose.position.x = list_maximum_value_y_indices[counter];
-                    pose.pose.position.y = list_maximum_value_x_indices[counter];
-                    pose.pose.position.z = 2;
-                    counter += 1;
-                }
-                else
-                {
-                    cout << "RTL" << endl;
-                    pose.pose.position.x = 1;
-                    pose.pose.position.y = 1;
-                    pose.pose.position.z = 1;
+                    cout << "Distance < 0.5" << endl;
+                    if (counter[UAV_COUNTER] < (y_max * x_max))
+                    {
+                        cout << "Counter" << counter[UAV_COUNTER] << endl;
+                        cout << "UAV_COUNTER: " << UAV_COUNTER << " "
+                             << "Maximum_Value_X_Indices: " << counter[UAV_COUNTER] << " " << list_maximum_value_x_indices[counter[UAV_COUNTER]] << endl;
+                        cout << "UAV_COUNTER: " << UAV_COUNTER << " "
+                             << "Maximum_Value_Y_Indices: " << counter[UAV_COUNTER] << " " << list_maximum_value_y_indices[counter[UAV_COUNTER]] << endl;
+                        pose[UAV_COUNTER].pose.position.x = list_maximum_value_y_indices[counter[UAV_COUNTER]];
+                        pose[UAV_COUNTER].pose.position.y = list_maximum_value_x_indices[counter[UAV_COUNTER]];
+                        pose[UAV_COUNTER].pose.position.z = 2;
+                        counter[UAV_COUNTER] += 1;
+                    }
+                    else
+                    {
+                        cout << "RTL" << endl;
+                        pose[UAV_COUNTER].pose.position.x = 1;
+                        pose[UAV_COUNTER].pose.position.y = 1;
+                        pose[UAV_COUNTER].pose.position.z = 1;
+                    }
                 }
             }
-        }
-        if (current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0)))
-        {
-            if (set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent)
+            if (current_state[UAV_COUNTER].mode != "OFFBOARD" &&
+                (ros::Time::now() - last_request[UAV_COUNTER] > ros::Duration(5.0)))
             {
-                ROS_INFO("Offboard enabled");
-            }
-            last_request = ros::Time::now();
-        }
-        else
-        {
-            if (!current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0)))
-            {
-                if (arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success)
+                if (set_mode_client[UAV_COUNTER].call(offb_set_mode[UAV_COUNTER]) &&
+                    offb_set_mode[UAV_COUNTER].response.mode_sent)
                 {
-                    ROS_INFO("Vehicle armed");
+                    ROS_INFO("Offboard enabled");
                 }
-                last_request = ros::Time::now();
+                last_request[UAV_COUNTER] = ros::Time::now();
             }
+            else
+            {
+                if (!current_state[UAV_COUNTER].armed &&
+                    (ros::Time::now() - last_request[UAV_COUNTER] > ros::Duration(5.0)))
+                {
+                    if (arming_client[UAV_COUNTER].call(arm_cmd[UAV_COUNTER]) &&
+                        arm_cmd[UAV_COUNTER].response.success)
+                    {
+                        ROS_INFO("Vehicle armed");
+                    }
+                    last_request[UAV_COUNTER] = ros::Time::now();
+                }
+            }
+
+            local_pos_pub[UAV_COUNTER].publish(pose[UAV_COUNTER]);
+
+            ros::spinOnce();
+            rate.sleep();
         }
-
-        local_pos_pub.publish(pose);
-
-        ros::spinOnce();
-        rate.sleep();
     }
     return 0;
 }
