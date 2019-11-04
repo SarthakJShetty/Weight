569e720bd63e504f28fa2d73a745ef93df4b2f52
SarthakJShetty <sarthakshetty97@gmail.com>
2019-11-04 16:14:33 +0530

    Integrated a sleep delay here that triggers the /mavros/state topic 5 seconds after the roscore and mavros node start.

--- environmentlaunch.sh
+++ environmentlaunch.sh
@@ -2,4 +2,5 @@ cd ~/catkin_ws/
 source devel/setup.bash
 gnome-terminal -e "bash -c \"roscore; exec bash\""
 gnome-terminal -e "bash -c \"rosrun mavros mavros_node _fcu_url:=/dev/ttyUSB0; exec bash\""
+sleep 5
 gnome-terminal -e "bash -c \"rostopic echo /mavros/state; exec bash\""
