d302599652ea6070bbf12ba063c0681299ed709a
SarthakJShetty <sarthakshetty97@gmail.com>
2019-11-04 15:59:31 +0530

    Made a more general sourcing and cd statements here. If the script is triggered from outside the src directory, unecessary paths get added to the ENVIRONMENT. Hence first cd'ing into the directory, then sourcing it.

--- environmentlaunch.sh
+++ environmentlaunch.sh
@@ -1,4 +1,5 @@
-source ~/catkin_ws/devel/setup.bash
+cd ~/catkin_ws/
+source devel/setup.bash
 gnome-terminal -e "bash -c \"roscore; exec bash\""
 gnome-terminal -e "bash -c \"rosrun mavros mavros_node _fcu_url:=/dev/ttyUSB0; exec bash\""
 gnome-terminal -e "bash -c \"rostopic echo /mavros/state; exec bash\""
