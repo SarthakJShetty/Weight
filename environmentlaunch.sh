2e276bd5b7a644094c0637824a82a2c8b357a2f7
SarthakJShetty <sarthakshetty97@gmail.com>
2019-11-15 16:06:13 +0530

    Adopting the timers for execution on laptop as well, which isn't as fast as the workstation.

--- environmentlaunch.sh
+++ environmentlaunch.sh
@@ -9,9 +9,10 @@ cd ~/catkin_ws/
 source devel/setup.bash
 #Initializing the roscore here for the MAVROS node to connect to
 gnome-terminal -e "bash -c \"roscore; exec bash\""
+sleep 10
 #Triggering the mavros_node here, connecting to the USB port where the transmitter here
 gnome-terminal -e "bash -c \"rosrun mavros mavros_node _fcu_url:=/dev/ttyUSB0; exec bash\""
 #The code sleeps for a bit before publishing the state of the mavros connection. The mavros_node takes a bit of time to connect to the master
-sleep 5
+sleep 10
 #Subscribing to the mavros_node state
 gnome-terminal -e "bash -c \"rostopic echo /mavros/state; exec bash\""
