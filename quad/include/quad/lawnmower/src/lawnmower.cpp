f793315ddc3aefa322260702a1870b78ac709ca4
SarthakJShetty <sarthakshetty97@gmail.com>
2019-11-18 12:11:46 +0530

    Commentted the lawn-mower code that was integrated earlier. Also, split the code into two bits, one for generation of the arrays and the other for change the priorities of exploration. Much cleaner now.

--- quad/include/quad/lawnmower/src/lawnmower.cpp
+++ quad/include/quad/lawnmower/src/lawnmower.cpp
@@ -1,8 +1,14 @@
+/*These functions serve two main purposes:
+1. Generate the list of points to be searched by lawn-mower.
+2. Generate the prioritized list of points in lawn-mower pattern.*/
+
 #include "lawnmower.hpp"
 
-int lawn_mower_generator_function(int y_max, int x_max, int uav_x_position, int uav_y_position, int list_lawn_mower_x_indices[], int list_lawn_mower_y_indices[], int pre_list_lawn_mower_x_indices[], int pre_list_lawn_mower_y_indices[], int lawn_mower_element_cycler)
+int lawn_mower_initializer_function(int y_max, int x_max, int pre_list_lawn_mower_x_indices[], int pre_list_lawn_mower_y_indices[], int lawn_mower_element_cycler)
 {
-    //This loop generates the set of the points, which will be manipulated to give the lawn-mower pattern
+    /*This function generates the set of the points, which will be manipulated to give the lawn-mower pattern in a new list(s) called the lawn_mower_indices*/
+
+    //Element cycler generates a linear set of points for further manipulation
     while (lawn_mower_element_cycler < (x_max * y_max))
     {
         for (int i = 0; i < x_max; i++)
@@ -15,15 +21,20 @@ int lawn_mower_generator_function(int y_max, int x_max, int uav_x_position, int 
             }
         }
     }
+}
+
+int lawn_mower_generator_function(int y_max, int x_max, int uav_x_position, int uav_y_position, int list_lawn_mower_x_indices[], int list_lawn_mower_y_indices[], int pre_list_lawn_mower_x_indices[], int pre_list_lawn_mower_y_indices[], int lawn_mower_element_cycler)
+{
+    lawn_mower_initializer_function(y_max, x_max, pre_list_lawn_mower_x_indices, pre_list_lawn_mower_y_indices, lawn_mower_element_cycler);
+    /*This function manipulates the pre_list to give the distinct lawn-mower pattern*/
 
     //This variable keeps track of which row is currently being accessed by the function written below
     lawn_mower_element_cycler = 0;
-
     while (lawn_mower_element_cycler < (x_max * y_max))
     {
         if (lawn_mower_element_cycler != ((y_max * x_max) - 1))
         {
-            cout << "Entering this mess: "
+            cout << "Entering the loop: "
                  << "Element Cycler: " << lawn_mower_element_cycler << endl;
             if (pre_list_lawn_mower_y_indices[lawn_mower_element_cycler] == y_max)
             {
@@ -33,7 +44,7 @@ int lawn_mower_generator_function(int y_max, int x_max, int uav_x_position, int 
                     cout << "Edge of the row which is at a turning point as well" << endl;
                     list_lawn_mower_x_indices[lawn_mower_element_cycler] = pre_list_lawn_mower_x_indices[lawn_mower_element_cycler];
                     list_lawn_mower_y_indices[lawn_mower_element_cycler] = pre_list_lawn_mower_y_indices[lawn_mower_element_cycler];
-                    cout << "Here 1: " << list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << list_lawn_mower_y_indices[lawn_mower_element_cycler] << ", NEXT:" << pre_list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << pre_list_lawn_mower_y_indices[lawn_mower_element_cycler] << endl;
+                    /*cout << "Here 1: " << list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << list_lawn_mower_y_indices[lawn_mower_element_cycler] << ", NEXT:" << pre_list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << pre_list_lawn_mower_y_indices[lawn_mower_element_cycler] << endl;*/
                     lawn_mower_element_cycler += 1;
                     int lawn_lawn_mower_element_cycler = lawn_mower_element_cycler;
                     for (int row_iterator = 0; row_iterator < y_max; row_iterator++)
@@ -50,7 +61,7 @@ int lawn_mower_generator_function(int y_max, int x_max, int uav_x_position, int 
                 {
                     list_lawn_mower_x_indices[lawn_mower_element_cycler] = pre_list_lawn_mower_x_indices[lawn_mower_element_cycler];
                     list_lawn_mower_y_indices[lawn_mower_element_cycler] = pre_list_lawn_mower_y_indices[lawn_mower_element_cycler];
-                    cout << "Here 3: " << list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << list_lawn_mower_y_indices[lawn_mower_element_cycler] << ", NEXT:" << pre_list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << pre_list_lawn_mower_y_indices[lawn_mower_element_cycler] << endl;
+                    /*cout << "Here 3: " << list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << list_lawn_mower_y_indices[lawn_mower_element_cycler] << ", NEXT:" << pre_list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << pre_list_lawn_mower_y_indices[lawn_mower_element_cycler] << endl;*/
                     lawn_mower_element_cycler += 1;
                 }
             }
@@ -58,7 +69,7 @@ int lawn_mower_generator_function(int y_max, int x_max, int uav_x_position, int 
             {
                 list_lawn_mower_x_indices[lawn_mower_element_cycler] = pre_list_lawn_mower_x_indices[lawn_mower_element_cycler];
                 list_lawn_mower_y_indices[lawn_mower_element_cycler] = pre_list_lawn_mower_y_indices[lawn_mower_element_cycler];
-                cout << "Here 4: " << list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << list_lawn_mower_y_indices[lawn_mower_element_cycler] << ", NEXT:" << pre_list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << pre_list_lawn_mower_y_indices[lawn_mower_element_cycler] << endl;
+                /*cout << "Here 4: " << list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << list_lawn_mower_y_indices[lawn_mower_element_cycler] << ", NEXT:" << pre_list_lawn_mower_x_indices[lawn_mower_element_cycler] << ", " << pre_list_lawn_mower_y_indices[lawn_mower_element_cycler] << endl; */
                 lawn_mower_element_cycler += 1;
             }
         }
