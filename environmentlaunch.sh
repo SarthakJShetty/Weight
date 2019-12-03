483314f4c158837757ba3131f6cf2004c3ef4b47
SarthakJShetty <sarthakshetty97@gmail.com>
2019-12-03 15:16:49 +0530

    Creating new windows for mavros/state, triggering the quad_node and sourcing the environment space as well.

--- environmentlaunch.sh
+++ environmentlaunch.sh
@@ -3,4 +3,8 @@ source ~/catkin_ws/devel/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
-roslaunch px4 multi_uav_mavros_sitl.launch
+gnome-terminal -e "bash -c \"roslaunch px4 multi_uav_mavros_sitl.launch; exec bash\""
+sleep 10
+gnome-terminal -e "bash -c \"rosrun quad quad_node; exec bash\""
+sleep 10
+gnome-terminal -e "bash -c \"rostopic echo /uav0/mavros/state; exec bash\""
