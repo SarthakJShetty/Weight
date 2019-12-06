c06e1b2bbec7ae638b8c340f319cdd29166618aa
SarthakJShetty <sarthakshetty97@gmail.com>
2019-12-06 15:31:41 +0530

    Added a time tracker to the top which notes how long the code subscribed to the relevant topics.

--- quad/include/quad/plotter/grapher.py
+++ quad/include/quad/plotter/grapher.py
@@ -6,6 +6,9 @@ def file_reader(position_file_txt, plotting_parameter):
     position_file = open(position_file_txt)
     old_second = 0
     current_second = 0
+    start_second = 0
+    end_second = 0
+    start_tracker = 0
     file_elements = []
     x_points = []
     y_points = []
@@ -16,7 +19,11 @@ def file_reader(position_file_txt, plotting_parameter):
 
     for line in file_elements:
         if "SECONDS" in line:
-            current_second = line
+            if(start_tracker==0):
+                start_second = int(line.split(":")[1])
+                start_tracker = 1
+            else:
+                current_second = line
         else:
             pass
         if((current_second != old_second) and (plotting_parameter == 'pos')):
@@ -39,7 +46,8 @@ def file_reader(position_file_txt, plotting_parameter):
             old_second = current_second
         else:
             pass
-
+    end_second = int(current_second.split(":")[1])
+    print("This is the end seconds: " + str(end_second) + " This is the start second: " + str(start_second))
     x_points_length = len(x_points)
     y_points_length = len(y_points)
     z_points_length = len(z_points)
@@ -57,12 +65,12 @@ def file_reader(position_file_txt, plotting_parameter):
     for time in range(0, z_points_length):
         z_time.append(time)
 
-    return x_points, y_points, z_points, x_time, y_time, z_time
+    return x_points, y_points, z_points, x_time, y_time, z_time, start_second, end_second
 
 
-x_points_1, y_points_1, z_points_1, x_time_1, y_time_1, z_time_1 = file_reader(
+x_points_1, y_points_1, z_points_1, x_time_1, y_time_1, z_time_1, start_second_1, end_second_1 = file_reader(
     'uav1_pos.txt', 'pos')
-x_points_2, y_points_2, z_points_2, x_time_2, y_time_2, z_time_2 = file_reader(
+x_points_2, y_points_2, z_points_2, x_time_2, y_time_2, z_time_2, start_second_1, end_second_2 = file_reader(
     'uav2_pos.txt', 'pos')
 
 plt.plot(x_time_1, x_points_1)
@@ -98,8 +106,9 @@ ax.plot([x_points_2[0]], [y_points_2[0]], [z_points_2[0]],
         label='Survivor\'s Start Point', color='g', marker='D')
 ax.plot([x_points_2[len(x_points_2)-1]], [y_points_2[len(x_points_2)-1]], [z_points_2[len(x_points_2)-1]],
         label='Survivor\'s End Point', color='r', marker='D')
-
-plt.title('UAV & Survivor Paths: Lawn-Mower Search', loc='left')
+difference = (end_second_1-start_second_1)
+print(difference)
+plt.title('UAV & Survivor Paths: Probabilistic Search. \nCompleted in: %s seconds' %difference, loc='left')
 ax.set_xlabel('X-Coordinate')
 ax.set_ylabel('Y-Coordinate')
 ax.set_zlabel('Z-Coordinate')
