ec731af35b4bff850d0b62443f5733143ed64ae3
SarthakJShetty <sarthakshetty97@gmail.com>
2019-12-17 17:24:49 +0530

    Included comments about the different plot functions (because I was clueless). Also created a new plot to mark the boundaries of the environment as well.

--- quad/include/quad/plotter/grapher.py
+++ quad/include/quad/plotter/grapher.py
@@ -19,7 +19,7 @@ def file_reader(position_file_txt, plotting_parameter):
 
     for line in file_elements:
         if "SECONDS" in line:
-            if(start_tracker==0):
+            if(start_tracker == 0):
                 start_second = int(line.split(":")[1])
                 start_tracker = 1
             else:
@@ -33,7 +33,7 @@ def file_reader(position_file_txt, plotting_parameter):
                 round(float(file_elements[file_elements.index(line)+2][:-1]), 2))
             z_points.append(
                 round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
-            #print(round(float(file_elements[file_elements.index(line)+1][:-1]), 2), round(float(file_elements[file_elements.index(line)+2][:-1]), 2), round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
+            # print(round(float(file_elements[file_elements.index(line)+1][:-1]), 2), round(float(file_elements[file_elements.index(line)+2][:-1]), 2), round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
             old_second = current_second
         elif((current_second != old_second) and ((plotting_parameter == 'vel') or (plotting_parameter == 'acc'))):
             x_points.append(
@@ -42,12 +42,13 @@ def file_reader(position_file_txt, plotting_parameter):
                 abs(round(float(file_elements[file_elements.index(line)+2][:-1]), 2)))
             z_points.append(
                 abs(round(float(file_elements[file_elements.index(line)+3][:-1]), 2)))
-            #print(round(float(file_elements[file_elements.index(line)+1][:-1]), 2), round(float(file_elements[file_elements.index(line)+2][:-1]), 2), round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
+            # print(round(float(file_elements[file_elements.index(line)+1][:-1]), 2), round(float(file_elements[file_elements.index(line)+2][:-1]), 2), round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
             old_second = current_second
         else:
             pass
     end_second = int(current_second.split(":")[1])
-    print("This is the end seconds: " + str(end_second) + " This is the start second: " + str(start_second))
+    print("This is the end seconds: " + str(end_second) +
+          " This is the start second: " + str(start_second))
     x_points_length = len(x_points)
     y_points_length = len(y_points)
     z_points_length = len(z_points)
@@ -91,24 +92,49 @@ plt.ylabel("X/Y/Z Position")
 plt.legend(['X Position', 'Y Position', 'Z Position'])
 plt.show()
 
+'''These points are required to plot the boundaries of the environment in the 3D plot'''
+x_points_4 = [0, 20.5, 20.5, 0, 0]
+y_points_4 = [0, 0, 20.5, 20.5, 0]
+z_points_4 = [0, 0, 0, 0, 0]
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+'''Plotting the entire trajectory of the UAV in the environment'''
 ax.plot(x_points_1, y_points_1, z_points_1,
         label='UAV 1 Trajectory', color='r')
+
+'''Plotting only the start point of the UAV trajectory'''
 ax.plot([x_points_1[0]], [y_points_1[0]], [z_points_1[0]],
         label='UAV 1 Start Point', color='g', marker='o')
+
+'''Plotting only the end point of the UAV trajectory'''
 ax.plot([x_points_1[len(x_points_1)-1]], [y_points_1[len(x_points_1)-1]], [z_points_1[len(x_points_1)-1]],
         label='UAV 1 End Point', color='r', marker='o')
+
+'''Plotting the entire trajectory of the survivors in the environment'''
 ax.plot(x_points_2, y_points_2, z_points_2,
         label='Survivor\'s Trajectory', color='b')
+
+'''Plotting only the start point of the survivors trajectory'''
 ax.plot([x_points_2[0]], [y_points_2[0]], [z_points_2[0]],
         label='Survivor\'s Start Point', color='g', marker='D')
+
+'''Plotting only the end point of the survivors trajectory'''
 ax.plot([x_points_2[len(x_points_2)-1]], [y_points_2[len(x_points_2)-1]], [z_points_2[len(x_points_2)-1]],
         label='Survivor\'s End Point', color='r', marker='D')
+
+'''Plotting the environment boundary'''
+ax.plot(x_points_4, y_points_4, z_points_4,
+        label='Environment Boundary', color='b', marker='>')
+
+'''Calculating the difference between the observer node has been triggered till when the UAV returns to the base'''
 difference = (end_second_1-start_second_1)
-print(difference)
-plt.title('UAV & Survivor Paths: Probabilistic Search. \nCompleted in: %s seconds' %difference, loc='left')
+
+print('Completed in: %s seconds: ' %(str(difference)))
+
+plt.title('UAV & Survivor Paths: Probabilistic Search. \nCompleted in: %s seconds' %
+          difference, loc='left')
 ax.set_xlabel('X-Coordinate')
 ax.set_ylabel('Y-Coordinate')
 ax.set_zlabel('Z-Coordinate')
