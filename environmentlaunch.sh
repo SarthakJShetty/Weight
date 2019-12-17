a121f88f75783d0bd785acc9b1fb83a51122f883
SarthakJShetty <sarthakshetty97@gmail.com>
2019-12-17 15:24:39 +0530

    Using tmux to manage windows, will not be launching them on multiple terminal windows.

--- environmentlaunch.sh
+++ environmentlaunch.sh
@@ -3,6 +3,4 @@ source ~/catkin_ws/devel/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
-gnome-terminal -e "bash -c \"roslaunch px4 multi_uav_mavros_sitl.launch; exec bash\""
-sleep 10
-gnome-terminal -e "bash -c \"rosrun quad quad_node; exec bash\""
+roslaunch px4 multi_uav_mavros_sitl.launch
