d71cb3debd66423888966f4e3fed4f233af4aaf6
SarthakJShetty <sarthakshetty97@gmail.com>
2019-12-23 18:30:25 +0530

    New .sh file to build before running the environment and check the config file for the environment as well.

--- environmentlaunch.sh
+++ environmentlaunch.sh
@@ -1,4 +1,8 @@
+cd ~/catkin_ws/
+catkin build
 cd ~/src/Firmware
+rm /home/sarthak/src/Firmware/launch/multi_uav_mavros_sitl.launch
+cp /home/sarthak/catkin_ws/src/config/multi_uav_mavros_sitl.launch /home/sarthak/src/Firmware/launch/
 source ~/catkin_ws/devel/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
