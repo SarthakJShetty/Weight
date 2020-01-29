bb6f893b9473c39fb5eed10bbc85daa7d04bfeae
SarthakJShetty <sarthakshetty97@gmail.com>
2020-01-29 13:02:04 +0530

    Changed the color scheme to 'viridis' since it's more legible than 'jet'.

--- quad/include/quad/plotter/densityplotter.py
+++ quad/include/quad/plotter/densityplotter.py
@@ -54,7 +54,7 @@ for weight_file in weight_files:
                     density[y_element, x_element] = by_element
 
     '''Importing a colormap here (Jet!) and reversing it to better infer the priority of the waypoints.'''
-    colormap = plt.cm.get_cmap('jet_r')
+    colormap = plt.cm.get_cmap('viridis')
 
     '''Plotting the weights here using matplotlib's colormesh functionality. Setting the colormap to 'jet' to ensure better contrast between smaller magnitudes.'''
     plt.pcolormesh(density, cmap= colormap)
