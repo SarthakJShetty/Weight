0bdd1055d6c064ca1a40c3ae8acedb69470cb635
SarthakJShetty <sarthakshetty97@gmail.com>
2020-02-03 11:37:29 +0530

    Comments! Added them here finally.

--- quad/include/quad/plotter/grapher.py
+++ quad/include/quad/plotter/grapher.py
@@ -1,3 +1,14 @@
+'''In this script, we plot the agent's position as well their trajectory from the .txt files that the subber.py script dumps during the subscribiction
+process. We plot:
+1. Position (X, Y, Z) vs Time
+2. 3D Trajectory of all agents involved in the simulation
+
+To-Do:
+1. Dump CSVs instead of .txt files
+
+-Sarthak
+(03/02/2020)'''
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -8,39 +19,54 @@ def second_converter(time_in_decimal):
     We compute the pure second format for both the start and the end second, then compute the difference to get the time elapsed.'''
 
     split_time = time_in_decimal.split(".")
+    '''Converting the individual units to integers and then to seconds'''
     time_hours = int(split_time[0]) * 60 * 60 * 1
     time_minute = int(split_time[1]) * 60 * 1
     time_second = int(split_time[2]) * 1
+
+    '''Returning the total number of seconds here'''
     return (time_hours + time_minute + time_second)
 
 
-def file_reader(position_file_txt, plotting_parameter):
-    position_file = open(position_file_txt)
+def file_reader(coordinates_file_txt, plotting_parameter):
+    '''This function reads the coordinates_file_text and depending on the plotting_parameter, charts either the position, velocity or acceleration.'''
+    position_file = open(coordinates_file_txt)
+    '''Keeps track of the previous second, so that multiple datapoints belonging to the same second aren't plotted on the chart'''
     old_second = 0
+    '''Keeps track of the current second noted in the data file. References it during plotting and compares it to the previous second to make sure
+    multiple datapoints of the same second aren't plotted.'''
     current_second = 0
+    '''Notes the first second recorded in the txt file, used to compute the time elapsed through the course of the simulation.'''
     start_second = 0
+    '''Last recorded second in the given agents data file'''
     end_second = 0
+    '''A variable to keep track of whether or not the first second has been recorded or not'''
     start_tracker = 0
-    start_second_decimal = 0
-    end_second_decimal = 0
+    '''List to hold all the elements of the file, so that they can be accessed easily through the course of the program execution'''
     file_elements = []
+    '''Lists to hold the X, Y and Z coordinates respectively'''
     x_points = []
     y_points = []
     z_points = []
+    '''Reading the lines in the text files and holding it in the file_elements list'''
     for line in position_file:
         if line != '\n':
             file_elements.append(line)
-
+    '''Traversing the elements of file_elements and checking if it contains the SECONDS keyword to grab the time of data collection'''
     for line in file_elements:
         if "SECONDS" in line:
+            '''Noting the start_second here'''
             if(start_tracker == 0):
                 start_second = (line.split(":")[1])
+                '''Sending it over to the converter which takes the raw time and converts it into seconds'''
                 start_second_decimal = second_converter(start_second)
                 start_tracker = 1
             else:
+                '''If the start_second has already been recorded continue to read the lines which have SECONDS in them'''
                 current_second = line
         else:
             pass
+        '''Adding recorded positions to the X, Y, Z point lists, either for position, velocity or acceleration plotting.'''
         if((current_second != old_second) and (plotting_parameter == 'pos')):
             x_points.append(
                 round(float(file_elements[file_elements.index(line)+1][:-1]), 2))
@@ -48,7 +74,6 @@ def file_reader(position_file_txt, plotting_parameter):
                 round(float(file_elements[file_elements.index(line)+2][:-1]), 2))
             z_points.append(
                 round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
-            # print(round(float(file_elements[file_elements.index(line)+1][:-1]), 2), round(float(file_elements[file_elements.index(line)+2][:-1]), 2), round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
             old_second = current_second
         elif((current_second != old_second) and ((plotting_parameter == 'vel') or (plotting_parameter == 'acc'))):
             x_points.append(
@@ -57,16 +82,17 @@ def file_reader(position_file_txt, plotting_parameter):
                 abs(round(float(file_elements[file_elements.index(line)+2][:-1]), 2)))
             z_points.append(
                 abs(round(float(file_elements[file_elements.index(line)+3][:-1]), 2)))
-            # print(round(float(file_elements[file_elements.index(line)+1][:-1]), 2), round(float(file_elements[file_elements.index(line)+2][:-1]), 2), round(float(file_elements[file_elements.index(line)+3][:-1]), 2))
             old_second = current_second
         else:
             pass
-
+    '''Recording the end second and converting it into pure seconds'''
     end_second = (current_second.split(":")[1])
     end_second_decimal = second_converter(end_second)
 
     print("This is the end seconds: " + str(end_second) +
           " This is the start second: " + str(start_second))
+    
+    '''Calculating the lengths of the lists to plot the axes of the respective plots'''
     x_points_length = len(x_points)
     y_points_length = len(y_points)
     z_points_length = len(z_points)
@@ -75,6 +101,7 @@ def file_reader(position_file_txt, plotting_parameter):
     y_time = []
     z_time = []
 
+    '''Creating the X-Axis for the time to be plotted'''
     for time in range(0, x_points_length):
         x_time.append(time)
 
@@ -84,6 +111,7 @@ def file_reader(position_file_txt, plotting_parameter):
     for time in range(0, z_points_length):
         z_time.append(time)
 
+    '''Returning all the variables recorded to the plotting functions'''
     return x_points, y_points, z_points, x_time, y_time, z_time, start_second, end_second, start_second_decimal, end_second_decimal
 
 
@@ -93,6 +121,8 @@ x_points_2, y_points_2, z_points_2, x_time_2, y_time_2, z_time_2, start_second_1
     'uav2_pos.txt', 'pos')
 x_points_3, y_points_3, z_points_3, x_time_3, y_time_3, z_time_3, start_second_3, end_second_3, start_second_decimal_3, end_second_decimal_3 = file_reader(
     'survivor_pos.txt', 'pos')
+
+'''Plotting the agents properties vs time '''
 
 plt.plot(y_time_1, y_points_1)
 plt.plot(x_time_1, x_points_1)
@@ -183,5 +213,7 @@ ax.set_xlabel('Y Axis')
 ax.set_ylabel('X Axis')
 ax.set_zlabel('Z Axis')
 ax.legend(loc='best', title='Trajectories', numpoints=1)
+
 plt.savefig('/home/sarthak/catkin_ws/src/assets/Trajectories.png')
+
 plt.show()
