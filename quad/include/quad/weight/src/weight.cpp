598b240373187e2516d0ca5650779e288d481353
SarthakJShetty <sarthakshetty97@gmail.com>
2020-02-10 15:50:38 +0530

    Missed out on the new change in directories for these two functions in weight.cpp.

--- quad/include/quad/weight/src/weight.cpp
+++ quad/include/quad/weight/src/weight.cpp
@@ -53,7 +53,7 @@ int weight_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y_m
 	//We then plot these weights as a density environment_map, to check the weightage of the waypoints even if we cannot plot them using the grapher.py function.
 
 	//The CSV of the environment_map generated is located at within the plotter package so that all entitities to be plotted are located in the same directory.
-	ofstream weightMapCSV("/home/sarthak/catkin_ws/src/quad/include/quad/plotter/weightMap.csv");
+	ofstream weightMapCSV("/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/weightMap.csv");
 	for (int j = 0; j < y_max; j++)
 	{
 		for (int i = 0; i < x_max; i++)
@@ -72,7 +72,7 @@ int priority_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y
 	// during the simulations.
 
 	//The CSV of the environment_map generated is located at within the plotter package so that all entitities to be plotted are located in the same directory.
-	ofstream priorityMapCSV("/home/sarthak/catkin_ws/src/quad/include/quad/plotter/priorityMap.csv");
+	ofstream priorityMapCSV("/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/priorityMap.csv");
 	for (int j = 0; j < y_max; j++)
 	{
 		for (int i = 0; i < x_max; i++)
