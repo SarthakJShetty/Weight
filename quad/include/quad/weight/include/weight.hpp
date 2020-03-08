1c10e31e36a8c1cee9dd521728c1333490e6a720
SarthakJShetty <sarthakshetty97@gmail.com>
2020-03-08 16:12:37 +0530

    Adding new weight.xpp functions here which have exploration dumpers built-in to the stack.

--- quad/include/quad/weight/include/weight.hpp
+++ quad/include/quad/weight/include/weight.hpp
@@ -5,25 +5,36 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <algorithm>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 //Boundaries of the flood environment
-const int x_max = 10;
-const int y_max = 10;
+const int x_max = 20;
+const int y_max = 20;
+const int grid_points = y_max * x_max;
 
 //Creating a structure which contains the weights assigned to each coordinate and an exploration status
 struct weighted_map
 {
     ///Weight is the value added to each coordinate for the weight-based method
     int weight;
-    //Exploration status for each cell 0-Unexplored; 1-Explored
+    //Including the priority of exploration of the specific waypoint as well
+    int priority;
+    //Exploration status for each cell 0-Unexplored; 1-Explored; 2-Survivor detected
     int exploration;
 };
 
-int weight_generator(weighted_map map[y_max][x_max], int x_max, int y_max);
-int exploration_generator(weighted_map map[y_max][x_max], int x_max, int y_max);
-int locator(weighted_map map[y_max][x_max], int x_max, int y_max, int uav_x_position, int uav_y_position);
-int weight_generator_function(int uav_x_position, int uav_y_position, int survivor_direction, int x_corner_coordinate_1, int x_corner_coordinate_2, int x_corner_coordinate_3, int x_corner_coordinate_4, int y_corner_coordinate_1, int y_corner_coordinate_2, int y_corner_coordinate_3, int y_corner_coordinate_4, int maximum_value, int map_priority[y_max][x_max], int weight_element_cycler, int list_maximum_value_x_indices[], int list_maximum_value_y_indices[]);
+int weighting_function(int uav_x_position, int uav_y_position, float &X_1, float &X_2, float &X_3, float &X_4, float &X_5, int n_x_difference, int n_y_difference, int n_set, int x_max, int y_max);
+int weight_initializer(weighted_map environment_map[y_max][x_max], int x_max, int y_max);
+int exploration_initializer(weighted_map environment_map[y_max][x_max], int x_max, int y_max);
+int locator(weighted_map environment_map[y_max][x_max], int x_max, int y_max, int uav_x_position, int uav_y_position);
+int weight_generator_function(int uav_x_position, int uav_y_position, float &X_1, float &X_2, float &X_3, float X_4, float &X_5, int n_x_difference, int n_y_difference, int n_set, int survivor_direction, int x_corner_coordinate_1, int x_corner_coordinate_2, int x_corner_coordinate_3, int x_corner_coordinate_4, int y_corner_coordinate_1, int y_corner_coordinate_2, int y_corner_coordinate_3, int y_corner_coordinate_4, int maximum_value, int weight_element_cycler, int list_maximum_value_x_indices[], int list_maximum_value_y_indices[]);
+int weight_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y_max);
+int priority_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y_max);
+int exploration_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y_max, int UAV_COUNTER);
--- quad/include/quad/weight/src/weight.cpp
+++ quad/include/quad/weight/src/weight.cpp
@@ -1,86 +1,179 @@
 #include "weight.hpp"
 
-int weight_generator(weighted_map map[y_max][x_max], int x_max, int y_max)
+int weight_initializer(weighted_map environment_map[y_max][x_max], int x_max, int y_max)
 {
-	//Initializing a map with 0 weights
+	//Initializing a environment_map with 0 weights
 	for (int j = 0; j < y_max; j++)
 	{
 		for (int i = 0; i < x_max; i++)
 		{
-			map[j][i].weight = 0;
+			environment_map[j][i].weight = 0;
 		}
 	}
 	return 0;
 }
 
-int exploration_generator(weighted_map map[y_max][x_max], int x_max, int y_max)
+int exploration_initializer(weighted_map environment_map[y_max][x_max], int x_max, int y_max)
 {
-	//Initializing the exploration component of the map structure
+	//Initializing the exploration component of the environment_map structure
 	for (int j = 0; j < y_max; j++)
 	{
 		for (int i = 0; i < x_max; i++)
 		{
-			map[j][i].exploration = 0;
+			environment_map[j][i].exploration = 0;
 		}
 	}
 	return 0;
 }
 
-int locator(weighted_map map[y_max][x_max], int x_max, int y_max, int uav_x_position, int uav_y_position)
+int locator(weighted_map environment_map[y_max][x_max], int x_max, int y_max, int uav_x_position, int uav_y_position)
 {
-	//Locating the UAV in the map generated
+	//Locating the UAV in the environment_map generated
 	for (int j = 0; j < y_max; j++)
 	{
 		for (int i = 0; i < x_max; i++)
 		{
 			if (i == (uav_x_position) && j == (uav_y_position))
 			{
-				cout << "*\t";
+				std::cout << "*\t";
 			}
 			else
 			{
-				cout << map[j][i].weight << "\t";
+				std::cout << environment_map[j][i].weight << "\t";
 			}
 		}
-		cout << endl;
+		std::cout << endl;
 	}
 	return 0;
 }
 
-int weight_generator_function(int uav_x_position, int uav_y_position, int survivor_direction, int x_corner_coordinate_1, int x_corner_coordinate_2, int x_corner_coordinate_3, int x_corner_coordinate_4, int y_corner_coordinate_1, int y_corner_coordinate_2, int y_corner_coordinate_3, int y_corner_coordinate_4, int maximum_value, int map_priority[y_max][x_max], int element_cycler, int list_maximum_value_x_indices[], int list_maximum_value_y_indices[])
+int weight_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y_max)
 {
-	weighted_map map[y_max][x_max];
-	weight_generator(map, x_max, y_max);
-	exploration_generator(map, x_max, y_max);
+	//This function takes the weightage environment_map, runs two for loops and dumps the corresponding weight environment_map to the disc, as a csv file.
+	//We then plot these weights as a density environment_map, to check the weightage of the waypoints even if we cannot plot them using the grapher.py function.
 
-	map[uav_y_position][uav_x_position].weight = 1000;
+	//The CSV of the environment_map generated is located at within the plotter package so that all entitities to be plotted are located in the same directory.
+	ofstream weightMapCSV("/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/weightMap.csv");
+	for (int j = 0; j < y_max; j++)
+	{
+		for (int i = 0; i < x_max; i++)
+		{
+			weightMapCSV << environment_map[j][i].weight << "\t";
+		}
+		weightMapCSV << "\n";
+	}
+	weightMapCSV.close();
+}
+
+int priority_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y_max)
+{
+	//This function takes the weightage environment_map, runs two for loops and dumps the corresponding weight environment_map to the disc, as a csv file.
+	//We then plot the priority of the weights as a density environment_map, to check the priority of the waypoints even if we cannot plot them using the grapher.py function from the locations of the UAV
+	// during the simulations.
+
+	//The CSV of the environment_map generated is located at within the plotter package so that all entitities to be plotted are located in the same directory.
+	ofstream priorityMapCSV("/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/priorityMap.csv");
+	for (int j = 0; j < y_max; j++)
+	{
+		for (int i = 0; i < x_max; i++)
+		{
+			priorityMapCSV << environment_map[j][i].priority << "\t";
+		}
+		priorityMapCSV << "\n";
+	}
+	priorityMapCSV.close();
+}
+
+int exploration_dumper(weighted_map environment_map[y_max][x_max], int x_max, int y_max, int UAV_COUNTER)
+{
+	//This function takes the weightage environment_map, runs two for loops and dumps the corresponding exploration status to the disc, as a csv file.
+	//We then plot the exploration status of each cell in the environment.
+
+	//The CSV of the environment_map generated is located at within the plotter package so that all entitities to be plotted are located in the same directory.
+	string initial_filename = "/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/explorationMap_";
+	stringstream str_UAV_COUNTER;
+	str_UAV_COUNTER << UAV_COUNTER;
+	string extension = ".csv";
+	string exploration_filename = initial_filename + str_UAV_COUNTER.str() + extension;
+
+	ofstream explorationMapCSV(exploration_filename.c_str());
+	for (int j = 0; j < y_max; j++)
+	{
+		for (int i = 0; i < x_max; i++)
+		{
+			explorationMapCSV << environment_map[j][i].exploration << "\t";
+		}
+		explorationMapCSV << "\n";
+	}
+	explorationMapCSV.close();
+}
+
+int weighting_function(int uav_x_position, int uav_y_position, float &X_1, float &X_2, float &X_3, float &X_4, float &X_5, int n_x_difference, int n_y_difference, int n_set, int x_max, int y_max)
+{
+	/*We've creating a new function to decide the weights to be assigned to each of the quadrants while generating weights. 
+	This function solves 2 problems:
+	1. No need to guess and chalk out the weights through trial and error.
+	2. Agnostic to the dimensions of the prototyping environment.*/
+
+	//uav_x/y_position is being subtracted from x/y_max since the boundary is the largest possible value of n_x/y_difference.
+
+	n_x_difference = x_max - uav_x_position;
+	n_y_difference = y_max - uav_y_position;
+
+	if (n_x_difference > n_y_difference)
+	{
+		n_set = n_x_difference;
+	}
+	else
+	{
+		n_set = n_y_difference;
+	}
+
+	X_1 = ((X_4 * pow(n_set, 3)) + pow(n_set, 1) + pow(n_set, 2) + pow(n_set, 3));
+	X_3 = ((X_1 - pow(n_set, 1)) - pow(n_set, 2)) / pow(n_set, 2);
+	X_2 = (X_1 - pow(n_set, 1)) / pow(n_set, 1);
+	X_5 = ((X_1 * n_set) + 1);
+}
+
+int weight_generator_function(int uav_x_position, int uav_y_position, float &X_1, float &X_2, float &X_3, float X_4, float &X_5, int n_x_difference, int n_y_difference, int n_set, int survivor_direction, int x_corner_coordinate_1, int x_corner_coordinate_2, int x_corner_coordinate_3, int x_corner_coordinate_4, int y_corner_coordinate_1, int y_corner_coordinate_2, int y_corner_coordinate_3, int y_corner_coordinate_4, int maximum_value, int weight_element_cycler, int list_maximum_value_x_indices[], int list_maximum_value_y_indices[])
+{
+	weighted_map environment_map[y_max][x_max];
+
+	//Initializes the default weights in each of the coordinates, since unitialized maps tend to be filled with garbage values.
+	weight_initializer(environment_map, x_max, y_max);
+	//Initializes the default exploration status of each, changing the default gibberish.
+	exploration_initializer(environment_map, x_max, y_max);
+
+	//This function generates the weights that have to be assigned to each quadrant of the environment.
+	weighting_function(uav_x_position, uav_y_position, X_1, X_2, X_3, X_4, X_5, n_x_difference, n_y_difference, n_set, x_max, y_max);
+
+	environment_map[uav_y_position][uav_x_position].weight = X_5;
 
 	//Visualizing the for loops as vectors helps. Y direction as vertical movement, X as horizontal movement
 	while ((x_corner_coordinate_1 != uav_x_position))
 	{
-		for (int j = uav_y_position; j >= y_corner_coordinate_1; j--)
+		for (int j = (uav_y_position - 1); j >= y_corner_coordinate_1; j--)
 		{
-			for (int i = (uav_x_position + 1); i < x_corner_coordinate_1; i++)
+			for (int i = (uav_x_position); i < x_corner_coordinate_1; i++)
 			{
-
 				if (survivor_direction == 1)
 				{
-					map[j][i].weight += 100;
+					environment_map[j][i].weight += X_1;
 				}
 				else if (survivor_direction == 2)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_2;
 				}
 				else if (survivor_direction == 3)
 				{
-					map[j][i].weight += 1;
+					environment_map[j][i].weight += X_4;
 				}
 				else if (survivor_direction == 4)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_3;
 				}
-				locator(map, x_max, y_max, uav_x_position, uav_y_position);
-				cout << endl;
+				//locator(environment_map, x_max, y_max, uav_x_position, uav_y_position);
+				//std::cout << endl;
 			}
 		}
 		x_corner_coordinate_1 -= 1;
@@ -89,28 +182,28 @@ int weight_generator_function(int uav_x_position, int uav_y_position, int surviv
 
 	while ((x_corner_coordinate_2 != uav_x_position))
 	{
-		for (int j = (uav_y_position - 1); j >= y_corner_coordinate_2; j--)
+		for (int j = (uav_y_position); j >= y_corner_coordinate_2; j--)
 		{
-			for (int i = (uav_x_position); i >= x_corner_coordinate_2; i--)
+			for (int i = (uav_x_position - 1); i >= x_corner_coordinate_2; i--)
 			{
 				if (survivor_direction == 1)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_3;
 				}
 				else if (survivor_direction == 2)
 				{
-					map[j][i].weight += 100;
+					environment_map[j][i].weight += X_1;
 				}
 				else if (survivor_direction == 3)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_2;
 				}
 				else if (survivor_direction == 4)
 				{
-					map[j][i].weight += 1;
+					environment_map[j][i].weight += X_4;
 				}
-				locator(map, x_max, y_max, uav_x_position, uav_y_position);
-				cout << endl;
+				//locator(environment_map, x_max, y_max, uav_x_position, uav_y_position);
+				//std::cout << endl;
 			}
 		}
 		x_corner_coordinate_2 += 1;
@@ -119,28 +212,28 @@ int weight_generator_function(int uav_x_position, int uav_y_position, int surviv
 
 	while ((x_corner_coordinate_3 != uav_x_position))
 	{
-		for (int j = uav_y_position; j < y_corner_coordinate_3; j++)
+		for (int j = (uav_y_position + 1); j < y_corner_coordinate_3; j++)
 		{
-			for (int i = (uav_x_position - 1); i >= x_corner_coordinate_3; i--)
+			for (int i = (uav_x_position); i >= x_corner_coordinate_3; i--)
 			{
 				if (survivor_direction == 1)
 				{
-					map[j][i].weight += 1;
+					environment_map[j][i].weight += X_4;
 				}
 				else if (survivor_direction == 2)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_3;
 				}
 				else if (survivor_direction == 3)
 				{
-					map[j][i].weight += 100;
+					environment_map[j][i].weight += X_1;
 				}
 				else if (survivor_direction == 4)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_2;
 				}
-				locator(map, x_max, y_max, uav_x_position, uav_y_position);
-				cout << endl;
+				//locator(environment_map, x_max, y_max, uav_x_position, uav_y_position);
+				//std::cout << endl;
 			}
 		}
 		x_corner_coordinate_3 += 1;
@@ -149,50 +242,55 @@ int weight_generator_function(int uav_x_position, int uav_y_position, int surviv
 
 	while ((x_corner_coordinate_4 != uav_x_position))
 	{
-		for (int j = (uav_y_position + 1); j < y_corner_coordinate_4; j++)
+		for (int j = (uav_y_position); j < y_corner_coordinate_4; j++)
 		{
-			for (int i = (uav_x_position); i < x_corner_coordinate_4; i++)
+			for (int i = (uav_x_position + 1); i < x_corner_coordinate_4; i++)
 			{
 				if (survivor_direction == 1)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_3;
 				}
 				else if (survivor_direction == 2)
 				{
-					map[j][i].weight += 1;
+					environment_map[j][i].weight += X_4;
 				}
 				else if (survivor_direction == 3)
 				{
-					map[j][i].weight += 2;
+					environment_map[j][i].weight += X_2;
 				}
 				else if (survivor_direction == 4)
 				{
-					map[j][i].weight += 100;
+					environment_map[j][i].weight += X_1;
 				}
-				locator(map, x_max, y_max, uav_x_position, uav_y_position);
-				cout << endl;
+				//locator(environment_map, x_max, y_max, uav_x_position, uav_y_position);
+				//std::cout << endl;
 			}
 		}
 		x_corner_coordinate_4 -= 1;
 		y_corner_coordinate_4 -= 1;
 	}
 
-	/*Finding the maximum element in the map
+	//Dumping the entire weightage environment_map to a .csv file to plot it
+	//We are not including it at the end of the functions since we nullify all weights after the environment_map population, to ensure
+	//that all weights have been prioritized and added to the list of exploration waypoints.
+	weight_dumper(environment_map, x_max, y_max);
+
+	/*Finding the maximum element in the environment_map
     1. Cycle through each row and find maximum
     2. If you find maximum store it as the maximum value
     3. If you encounter >= current maximum then append index (x, y) 
     4. Check the maximum value and remove it each row
-    5. Iterate through map again*/
+    5. Iterate through environment_map again*/
 
-	while (element_cycler <= (y_max * x_max))
+	while (weight_element_cycler <= (y_max * x_max))
 	{
 		for (int row_iterator = 0; row_iterator < y_max; row_iterator++)
 		{
 			for (int column_iterator = 0; column_iterator < x_max; column_iterator++)
 			{
-				if (map[row_iterator][column_iterator].weight > maximum_value)
+				if (environment_map[row_iterator][column_iterator].weight > maximum_value)
 				{
-					maximum_value = map[row_iterator][column_iterator].weight;
+					maximum_value = environment_map[row_iterator][column_iterator].weight;
 				}
 			}
 		}
@@ -201,38 +299,39 @@ int weight_generator_function(int uav_x_position, int uav_y_position, int surviv
 		{
 			for (int column_iterator_2 = 0; column_iterator_2 < x_max; column_iterator_2++)
 			{
-				if ((map[row_iterator_2][column_iterator_2].weight == maximum_value))
+				if ((environment_map[row_iterator_2][column_iterator_2].weight == maximum_value))
 				{
-					map[row_iterator_2][column_iterator_2].weight = 0;
-					element_cycler += 1;
+					environment_map[row_iterator_2][column_iterator_2].weight = 0;
+					weight_element_cycler += 1;
 
 					if (maximum_value != 0)
 					{
-						map_priority[row_iterator_2][column_iterator_2] = element_cycler;
-						list_maximum_value_x_indices[element_cycler - 1] = (column_iterator_2);
-						list_maximum_value_y_indices[element_cycler - 1] = (row_iterator_2);
+						environment_map[row_iterator_2][column_iterator_2].priority = weight_element_cycler;
+						list_maximum_value_x_indices[weight_element_cycler - 1] = (column_iterator_2);
+						list_maximum_value_y_indices[weight_element_cycler - 1] = (row_iterator_2);
 					}
 				}
 			}
 		}
 		maximum_value = 0;
-		locator(map, x_max, y_max, uav_x_position, uav_y_position);
-		cout << endl;
+		//locator(environment_map, x_max, y_max, uav_x_position, uav_y_position);
+		//std::cout << endl;
 	}
 
 	for (int i = 0; i < (y_max * x_max); i++)
 	{
-		cout << "Maximum Value X Index:" << list_maximum_value_x_indices[i] << endl;
-		cout << "Maximum Value Y Index:" << list_maximum_value_y_indices[i] << endl;
+		std::cout << "Maximum Value X Index:" << list_maximum_value_x_indices[i] << endl;
+		std::cout << "Maximum Value Y Index:" << list_maximum_value_y_indices[i] << endl;
 	}
 
-	for (int i = 0; i < y_max; i++)
+	for (int j = 0; j < y_max; j++)
 	{
-		for (int j = 0; j < x_max; j++)
+		for (int i = 0; i < x_max; i++)
 		{
-			cout << map_priority[i][j] << "\t";
+			std::cout << environment_map[j][i].priority << "\t";
 		}
-		cout << endl;
+		std::cout << endl;
 	}
+	priority_dumper(environment_map, x_max, y_max);
 	return 0;
 }
