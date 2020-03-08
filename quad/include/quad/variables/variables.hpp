6830d95fc5e7c2edfa9fff9a5cb96ca02c02c821
SarthakJShetty <sarthakshetty97@gmail.com>
2020-03-08 23:21:10 +0530

    New variables.hpp for new borrowed functionality from simulation code.

--- quad/include/quad/variables/variables.hpp
+++ quad/include/quad/variables/variables.hpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
-//Testing a theory here. Experimental!
-int global_pointer;
+//global_pointer points to the corresponding publisher/subscriber combination at the top of the ROS code while subscribing to the individual agent's parameters
+int *global_pointer;
 
 //Threshld to switch to next waypoint
-float dist_threshold = 0.3;
+float waypoint_dist_threshold = 0.5;
+
+//This variable is a threshold to check whether or not the UAV has reached the survivor's position or not
+//This value has been obtained by calculating the FOV assuming a 45° observer angle and a 2m height
+//In the future, we'll design a model that dynamically adapts this value to the height of the UAV
+float survivor_dist_threshold = 4.0;
 
 //Declaring the number of UAVs being used here
 const int N_UAV = 1;
@@ -16,11 +21,15 @@ const int N_UAV = 1;
 //Boundaries of the flood environment
 extern const int x_max;
 extern const int y_max;
+extern const int grid_points;
+weighted_map environment_map[y_max][x_max];
 
 //Subscribers and publishers declared here
 ros::Subscriber position_subscriber[N_UAV];
 ros::Subscriber state_sub[N_UAV];
 ros::Publisher local_pos_pub[N_UAV];
+ros::Publisher survivor_position_pub[N_UAV];
+ros::Publisher counter_pub[N_UAV];
 
 //Service clients to handle ROS services
 ros::ServiceClient arming_client[N_UAV];
@@ -34,12 +43,17 @@ ros::Subscriber cv_node[N_UAV];
 ros::Subscriber switch_node[N_UAV];
 
 //Generating an integer to store the binary value sent over by the CV node
-std_msgs::Int8 cv_msgs[N_UAV];
+std_msgs::Int32 cv_msgs[N_UAV];
 //This variable keeps track of which search pattern is being triggered
-std_msgs::Int8 switch_msgs[N_UAV];
+std_msgs::Int32 switch_msgs[N_UAV];
+//This counter is used to publish the counter value for diagnostics primarily
+std_msgs::Int32 counter_msgs[N_UAV];
 
 //Declaring the pose here for both UAVs
 geometry_msgs::PoseStamped pose[N_UAV];
+
+//Declaring the pose here for both UAVs
+geometry_msgs::PoseStamped survivor_pose[N_UAV];
 
 //State of the UAVs connected/non-connected
 mavros_msgs::State current_state[N_UAV];
@@ -61,22 +75,41 @@ float current_position_x[N_UAV];
 float current_position_y[N_UAV];
 float current_position_z[N_UAV];
 
-//This keeps track of the waypoint currently conveyed to the UAV. If = 100 stops
-int counter[N_UAV];
-
 //Distance keeps track of initial position and desired positon
-float dist;
+float waypoint_dist[N_UAV];
 
-//This is the survivor's x, y coordinate & direction
-int survivor_x_coordinate = 2;
-int survivor_y_coordinate = 2;
+//This variable keeps track of distance between UAV's position and the survivor's position. Then compared to survivor_dist_threshold
+float survivor_dist[N_UAV];
+
+//This is the survivor's x, y coordinate & direction. We use pointers to ensure consistancy across the files
+float start_survivor_x_coordinate = 10.0;
+float start_survivor_y_coordinate = 10.0;
+
+//Survivor coordinate is continuously updated from the start_survivor_coordinate
+float *survivor_x_coordinate = &start_survivor_x_coordinate;
+float *survivor_y_coordinate = &start_survivor_y_coordinate;
+
+//Introducing survivor coordinates as arrays so that coordinates are truly seperate for UAV i and UAV j ∀ i, j ∈ {0, N}
+float survivor_x_coordinate_array[N_UAV];
+float survivor_y_coordinate_array[N_UAV];
+
+//Velocity of the survivor
+float velocity = 0.3;
+//Time step to calculate the next set of survivor coordinates from the previous velocity
+float time_step = 1.0;
+
+//Incorporating time variables to keep track of how often the coordinates of the survivor are sent to quad_node
+int current_second = 0;
+//The start_previous_second is used to initialize the previous second pointer
+int start_previous_second = 0;
+int *previous_second = &start_previous_second;
 
 //Survivor's direction 1-> ++, 2-> -+, 3-> --, 4-> +-
 int survivor_direction = 4;
 
 //UAVs current position
-int uav_y_position = 5;
-int uav_x_position = 5;
+int uav_y_position = start_survivor_x_coordinate;
+int uav_x_position = start_survivor_y_coordinate;
 
 //Corner coordinates of each of the quadrants of exploration
 int x_corner_coordinate_1 = x_max;
@@ -91,14 +124,26 @@ int y_corner_coordinate_3 = y_max;
 int x_corner_coordinate_4 = x_max;
 int y_corner_coordinate_4 = y_max;
 
+//Initializing the weight pointers that will be used to assign weights to the different quadrants by the weight.xpp function
+float X_1 = 1.0;
+float X_2 = 1.0;
+float X_3 = 1.0;
+float X_4 = 1.0;
+float X_5 = 1.0;
+
+//These variables compute the number of iterations required along the X and Y axes to hit the boundaries of the environment
+int n_x_difference;
+int n_y_difference;
+int n_set;
+
 //Holds the maximum value that the code has come across in the map
 int maximum_value = 0;
 
 //Cycles through the elements of the weight map generated
 int weight_element_cycler = 0;
+//This element mowes through the pre-ordered list and interacts with the lawn_mower array
 int lawn_mower_element_cycler = 0;
-
-int map_priority[y_max][x_max];
+int lawn_lawn_mower_element_cycler = 0;
 
 //Creating lists to iterate through waypoints. Hypothesis is that sharing of vectors is causing seg faults.
 int list_maximum_value_x_indices[y_max * x_max];
@@ -113,3 +158,9 @@ int lawn_mower_trigger_check[N_UAV];
 
 //This variable makes sure that each UAV generates the weighted map only once
 int weight_trigger_check[N_UAV];
+
+//This variable checks whether each of the UAVs have found the survivor or not. If triggered to one, implies that that particular UAV has found the survivor
+int survivor_detection_check[N_UAV];
+
+//This array makes sure whether or not the explorationDump has taken place or not for the respective UAV
+int exploration_dump_check[N_UAV];
