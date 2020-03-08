174b5e8005d2dce5dd88c28a7d90bec747cb2549
SarthakJShetty <sarthakshetty97@gmail.com>
2020-03-08 23:23:59 +0530

    Adding the priorityMap plotter here as well. Adding the explorationMap plotter here as well.

---
+++ quad/include/quad/plotter/densityPlotter.py
@@ -0,0 +1,86 @@
+'''This script is written to plot the weightage map created as a density color mesh.
+While running the code, the drone does not execute the entire weightmap. To ensure that the weights are being generated correctly, we've
+built this function to analyze the weightage map generated as a matplotlib colormesh.
+
+Similar to the rest of the modules written for this project, this densityplotter function is agnostic to the dimensions of the environment.
+Tests are due to substantiate this claim. Will be updating a section in the README of repository dedicated to this.
+
+-Sarthak
+(20/12/2019)'''
+
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+'''Filename pointing to the weightMap csv file'''
+weight_files = ['/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/weightMap.csv', '/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/priorityMap.csv']
+
+for weight_file in weight_files:
+    '''Start row makes sure that the row-elements are counted only once.'''
+    start_row = 0
+    '''Variable to count the number of elements in each row.'''
+    row = 0
+    '''Variable to count the number of columns.'''
+    column = 0
+
+    with open(weight_file) as csv_file:
+        '''This loop is used to calculate the number of rows and columns in the csv file that has been dumped.
+        We use this data to intialize the numpy arrays that we use to plot the density maps.'''
+        read_file = csv.reader(csv_file, delimiter='\t')
+        for element in read_file:
+            column += 1
+            for by_element in element:
+                '''This check is to make sure that the number of row elements of only one row is calculated.'''
+                if start_row is 0:
+                    '''This if condition is requried because the delimiter condition in csv.reader creates a N+th element which is just '', a space '''
+                    if by_element is not '':
+                        row += 1
+            if start_row is 0:
+                '''After the number of row-elements has been counted, do not count anymore elements on successive columns, this variable keeps a track on that.'''
+                start_row += 1
+
+    '''Generating a numpy array to hold the density read from the .csv file.'''
+    density = np.zeros([column, row])
+
+    '''Using numpy arrays here for rows and columns, because colormesh takes input in these forms.'''
+    column_array = np.arange(0, column)
+    row_array = np.arange(0, row)
+
+    with open(weight_file) as csv_file:
+        read_file = csv.reader(csv_file, delimiter='\t')
+        for (y_element, element) in zip(column_array, read_file):
+            for (x_element, by_element) in zip(row_array, element):
+                if by_element is not '':
+                    density[y_element, x_element] = by_element
+    fig, ax = plt.subplots()
+    '''Importing a colormap here (Jet!) and reversing it to better infer the priority of the waypoints.'''
+    colormap = plt.cm.get_cmap('viridis')
+
+    '''Plotting the weights here using matplotlib's colormesh functionality. Setting the colormap to 'jet' to ensure better contrast between smaller magnitudes.'''
+    plt.pcolormesh(density, cmap=colormap)
+
+    '''Adding a colorbar to understand the value of the different weights & their intensity.'''
+    cb = plt.colorbar()
+
+    if 'weightMap.csv' in weight_file:
+        cb.set_label('Increasing Weightage of Waypoints')
+    elif 'priorityMap.csv' in weight_file:
+        cb.set_label('Decreasing Priority of Waypoints')
+
+    '''Flipping the coordinates here to ensure that ease of understanding between plots and the Gazebo environment.'''
+    plt.ylim(column, 0)
+
+    '''Adding a grid to improve readability of the map and anchors the colors generated as well.'''
+    plt.grid(True)
+
+    if weight_file.split('/')[-1] == 'weightMap.csv':
+        plt.title('Density of Weights')
+    elif weight_file.split('/')[-1] == 'priorityMap.csv':
+        plt.title('Priority of Waypoints')
+    
+    ax.set_aspect('equal')
+    
+    '''Splitting the filename at the extension, extracting only the first part and appending a png to it to save it to disc.'''
+    plt.savefig('/home/sarthak/catkin_ws/src/assets/' +
+                (weight_file.split('/')[-1]).split('.')[0]+'.png')
+    plt.show()
---
+++ quad/include/quad/plotter/mapPlotter.py
@@ -0,0 +1,93 @@
+'''This script is written to plot the exploration map that's being generated in real-time by the agents exploring the environment. The 2D map being generated 
+is part of the weighted_map structure which has a int member called exploration which gets cycled and dumped to the disc using the exploration_dumper() function,
+part of the weight.xpp package.
+
+This script is a fork of the densityPlotter.py, hence the similarity of the structure/comments.
+
+-Sarthak
+(09/02/2020)'''
+
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+from matplotlib import colors
+from matplotlib.patches import Patch
+
+'''Filename pointing to the explorationMap csv file'''
+map_files = ['/home/sarthak/catkin_ws/src/quad/include/quad/plotter/data/explorationMap_0.csv']
+
+for map_file in map_files:
+    '''Start row makes sure that the row-elements are counted only once.'''
+    start_row = 0
+    '''Variable to count the number of elements in each row.'''
+    row = 0
+    '''Variable to count the number of columns.'''
+    column = 0
+
+    with open(map_file) as csv_file:
+        '''This loop is used to calculate the number of rows and columns in the csv file that has been dumped.
+        We use this data to intialize the numpy arrays that we use to plot the density maps.'''
+        read_file = csv.reader(csv_file, delimiter='\t')
+        for element in read_file:
+            column += 1
+            for by_element in element:
+                '''This check is to make sure that the number of row elements of only one row is calculated.'''
+                if start_row is 0:
+                    '''This if condition is requried because the delimiter condition in csv.reader creates a N+th element which is just '', a space '''
+                    if by_element is not '':
+                        row += 1
+            if start_row is 0:
+                '''After the number of row-elements has been counted, do not count anymore elements on successive columns, this variable keeps a track on that.'''
+                start_row += 1
+
+    '''Generating a numpy array to hold the density read from the .csv file.'''
+    density = np.zeros([column, row])
+
+    '''Using numpy arrays here for rows and columns, because colormesh takes input in these forms.'''
+    column_array = np.arange(0, column)
+    row_array = np.arange(0, row)
+
+    with open(map_file) as csv_file:
+        read_file = csv.reader(csv_file, delimiter='\t')
+        for (y_element, element) in zip(column_array, read_file):
+            for (x_element, by_element) in zip(row_array, element):
+                if by_element is not '':
+                    density[y_element, x_element] = by_element
+
+    '''Here, we create a custom legend to label the 3 types of cell in the colormap being plotted below.'''
+    legend_elements = [Patch(facecolor='red', edgecolor='r',
+                         label='Unexplored Cell'),
+                   Patch(facecolor='blue', edgecolor='b',
+                         label='Explored Cell'),
+                   Patch(facecolor='yellow', edgecolor='y',
+                         label='Survivor Cell')]
+
+    '''Generating the subplot to extract the ax and the fig elements of the plot.'''
+    fig, ax = plt.subplots()
+
+    '''Generating a custom colormap of 3 colors to plot the exploration status of the different cells.'''
+    cmap = colors.ListedColormap(['red', 'blue', 'yellow'])
+
+    '''Plotting the weights here using matplotlib's colormesh functionality. Setting the colormap to 'jet' to ensure better contrast between smaller magnitudes.'''
+    plt.pcolormesh(density, cmap = cmap)
+
+    '''Flipping the coordinates here to ensure that ease of understanding between plots and the Gazebo environment.'''
+    plt.ylim(column, 0)
+
+    '''Adding a grid to improve readability of the map and anchors the colors generated as well.'''
+    plt.grid(True)
+
+    if map_file == 'explorationMap_0.csv' or 'explorationMap_1.csv':
+        plt.title('Exploration of Environment by UAV ' + ((map_file.split('/')[-1]).split('.'))[0].split('_')[1])
+
+    '''Adding labels to the X and the Y axis to improve the readability of the maps here'''
+    plt.xticks(row_array)
+    plt.yticks(column_array)
+    
+    ax.set_aspect('equal')
+    ax.legend(handles = legend_elements)
+
+    '''Splitting the filename at the extension, extracting only the first part and appending a png to it to save it to disc.'''
+    plt.savefig('/home/sarthak/catkin_ws/src/assets/' +
+                (map_file.split('/')[-1]).split('.')[0]+'.png')
+    plt.show()
