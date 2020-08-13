b992b30b3443b0a966bc6916644da35339ece2d5
SarthakJShetty <sarthakshetty97@gmail.com>
2020-08-13 23:19:53 +0530

    Final update to the observer_node before merging with master. Added comments regarding the array system and the reference of indices for UAV control.

--- quad/src/observer.cpp
+++ quad/src/observer.cpp
@@ -2,7 +2,6 @@
 for multi-UAV systems.
 Here, a constant streams of 1's are published when the code is triggered to resemble an observer detecting a survivor
 in it's vicinity.
-This is a temporary code, will replace it with a more robust solution for multiple UAV control.
 
 -Sarthak
 (19/11/2019)
@@ -24,28 +23,39 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "observer");
     ros::NodeHandle n;
 
+    //Declaring the number of UAVs here
+    int N_UAV = 2;
+
+    //Declaring the publisher here. Only a single topic to control the switch nodes of all UAVs at their respective indices.
     ros::Publisher switch_publisher_1 = n.advertise<std_msgs::Int32MultiArray>("/uav/switch_node", 1000);
-    // ros::Publisher switch_publisher_2 = n.advertise<std_msgs::Int16MultiArray>("/uav1/switch_node", 1000);
 
     //Lowering the rate here and seems like the erraticity of the model has been reduced. Need to debug this further.
     ros::Rate loop_rate(1);
+
+    //Here, we declare the array that gets published over the ROS Comms to the quad_node.
     std_msgs::Int32MultiArray msg_1;
+
+    //Specific UAV that has to be triggered by the observer node.
     int uav_to_trigger = 0;
+
     //This variable makes sure that the cout statement prints only 10 times once the node has been triggered.
     int print_counter = 0;
     while (ros::ok())
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < N_UAV; i++)
         {
             if(uav_to_trigger == i)
             {
+            //If the specifc UAVs index is encountered in the array, then 1 it.
             msg_1.data.push_back(1);
             }
             else
             {
+            //If the specifc UAVs index is not encountered in the array, then 0 it.
             msg_1.data.push_back(0);
             }
         }
+        //Publish the data to the publisher to the ROS Comms.
         switch_publisher_1.publish(msg_1);
         ros::spinOnce();
         loop_rate.sleep();
