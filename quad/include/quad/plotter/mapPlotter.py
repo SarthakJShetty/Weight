1d39466740930c3eea79f921fe460f30e464ac1b
SarthakJShetty <sarthakshetty97@gmail.com>
2020-11-24 22:07:59 +0530

    Finally! Scrapped some old messy code to figure out the rows and columns and replaced it with something more intuitive. Broke the main code into three functions, mapPresenter() to present each map, mapStitcher() to combine individual maps into one and individualMapGenerator() which generates a map from each UAVs dumped exploration_X.csv. Need to replace another bit of code which access the CSV files element. Need to fix change in colorscheme also.

--- quad/include/quad/plotter/mapPlotter.py
+++ quad/include/quad/plotter/mapPlotter.py
@@ -2,7 +2,7 @@
 is part of the weighted_map structure which has a int member called exploration which gets cycled and dumped to the disc using the exploration_dumper() function,
 part of the weight.xpp package.
 
-This script is a fork of the densityPlotter.py, hence the similarity of the structure/comments.
+This script is a fork of the individualMapPlotter.py, hence the similarity of the structure/comments.
 
 -Sarthak
 (09/02/2020)'''
@@ -14,83 +14,81 @@ from matplotlib import colors
 from matplotlib.patches import Patch
 import os.path
 
-'''Filename pointing to the explorationMap csv file'''
-
-'''This variable checks the number of UAV file data available on disc'''
-n_uav = 0
-
-'''This line checks the number of UAV/Survivor paits found in the disc'''
-while os.path.isfile('data/explorationMap_'+str(n_uav)+'.csv'):
-    n_uav+=1
-
-map_files = ['data/explorationMap_'+ str(uav) +'.csv' for uav in range(0, n_uav)]
-
-for map_file in map_files:
+def individualMapGenerator(map_file):
+    '''This function generates the map from the .CSV file read from the disc'''
     '''Start row makes sure that the row-elements are counted only once.'''
     start_row = 0
-    '''Variable to count the number of elements in each row.'''
-    row = 0
-    '''Variable to count the number of columns.'''
-    column = 0
 
-    with open(map_file) as csv_file:
-        '''This loop is used to calculate the number of rows and columns in the csv file that has been dumped.
-        We use this data to intialize the numpy arrays that we use to plot the density maps.'''
-        read_file = csv.reader(csv_file, delimiter='\t')
-        for element in read_file:
-            column += 1
-            for by_element in element:
-                '''This check is to make sure that the number of row elements of only one row is calculated.'''
-                if start_row is 0:
-                    '''This if condition is requried because the delimiter condition in csv.reader creates a N+th element which is just '', a space '''
-                    if by_element is not '':
-                        row += 1
-            if start_row is 0:
-                '''After the number of row-elements has been counted, do not count anymore elements on successive columns, this variable keeps a track on that.'''
+    with open(map_file) as element:
+        '''Iterating through each row of the CSV file here to figure out the rows and columns'''
+        mapObj = csv.reader(element, delimiter = '\t')
+        rows = 0
+        '''We need to count the column elements only once, since they remain consistent throughout the map'''
+        start_row = 0
+        columns = 0
+        for row_element in mapObj:
+            '''Iterating through the rows here'''
+            rows+=1
+            if start_row == 0:
+                '''If no row has been iterated through before this, then iterate through the row and figure out the number of column elements.
+                Iterate through it only once though.'''
                 start_row += 1
+                for column_element in row_element:
+                    if(column_element != ''):
+                        columns+=1
 
-    '''Generating a numpy array to hold the density read from the .csv file.'''
-    density = np.zeros([column, row])
+    '''Generating a numpy array to hold the individualMap read from the .csv file.'''
+    individualMap = np.zeros([rows, columns])
 
     '''Using numpy arrays here for rows and columns, because colormesh takes input in these forms.'''
-    column_array = np.arange(0, column)
-    row_array = np.arange(0, row)
+    column_array = np.arange(0, columns)
+    row_array = np.arange(0, rows)
 
     with open(map_file) as csv_file:
         read_file = csv.reader(csv_file, delimiter='\t')
         for (y_element, element) in zip(column_array, read_file):
             for (x_element, by_element) in zip(row_array, element):
                 if by_element is not '':
-                    density[y_element, x_element] = by_element
+                    individualMap[y_element, x_element] = by_element
+
+    return individualMap
+
+def mapPresenter(individualMap, map_file = None):
+    '''This function presents each UAVs map using matplotlib functions'''
+    if map_file == None:
+        '''If no map_file is presented, the function assumes that the stitched map is being presented here'''
+        map_file = 'data/explorationMap_All.csv'
 
     '''Here, we create a custom legend to label the 3 types of cell in the colormap being plotted below.'''
     legend_elements = [Patch(facecolor='red', edgecolor='r',
-                         label='Unexplored Cell'),
-                   Patch(facecolor='blue', edgecolor='b',
-                         label='Explored Cell'),
-                   Patch(facecolor='yellow', edgecolor='y',
-                         label='Survivor Cell')]
+                        label='Unexplored Cell'),
+                Patch(facecolor='blue', edgecolor='b',
+                        label='Explored Cell'),
+                Patch(facecolor='yellow', edgecolor='y',
+                        label='Survivor Cell')]
 
-    '''Generating the subplot to extract the ax and the fig elements of the plot.'''
-    fig, ax = plt.subplots()
+    '''Generating the subplot to extract the ax and the fig elements of the plot. Fig component isn't used though'''
+    _, ax = plt.subplots()
 
     '''Generating a custom colormap of 3 colors to plot the exploration status of the different cells.'''
     cmap = colors.ListedColormap(['red', 'blue', 'yellow'])
 
     '''Plotting the weights here using matplotlib's colormesh functionality. Setting the colormap to 'jet' to ensure better contrast between smaller magnitudes.'''
-    plt.pcolormesh(density, cmap = cmap)
+    plt.pcolormesh(individualMap, cmap = cmap)
+
+    rows, columns = individualMap.shape
 
     '''Flipping the coordinates here to ensure that ease of understanding between plots and the Gazebo environment.'''
-    plt.ylim(column, 0)
+    plt.ylim(columns, 0)
 
     '''Adding a grid to improve readability of the map and anchors the colors generated as well.'''
     plt.grid(True)
-    
-    plt.title('Exploration of Environment by UAV ' + ((map_file.split('/')[-1]).split('.'))[0].split('_')[1])
+
+    plt.title('Exploration of Environment by UAV: ' + ((map_file.split('/')[-1]).split('.'))[0].split('_')[-1])
 
     '''Adding labels to the X and the Y axis to improve the readability of the maps here'''
-    plt.xticks(row_array)
-    plt.yticks(column_array)
+    plt.xticks(np.arange(0, rows))
+    plt.yticks(np.arange(0, columns))
 
     '''Axes here are flipped because we follow the right hand screw rule in 3D.'''
     plt.xlabel('Y Axis')
@@ -103,3 +101,35 @@ for map_file in map_files:
     plt.savefig('/home/sarthak/catkin_ws/src/assets/' +
                 (map_file.split('/')[-1]).split('.')[0]+'.png')
     plt.show()
+
+def mapStitcher(individualMaps):
+    '''This function stitches the exploration maps generated together'''
+    rows, columns = individualMaps[0].shape
+    completeMap = np.zeros([rows, columns])
+    for individualMap in individualMaps:
+        '''Applyinf a .add function here to add each individualMap to the completeMap'''
+        completeMap = np.add(completeMap, individualMap)
+
+    return completeMap
+
+'''This variable checks the number of UAV file data available on disc'''
+n_uav = 0
+
+'''This line checks the number of UAV/Survivor paits found in the disc'''
+while os.path.isfile('data/explorationMap_'+str(n_uav)+'.csv'):
+    n_uav+=1
+
+map_files = ['data/explorationMap_'+ str(uav) +'.csv' for uav in range(0, n_uav)]
+
+individualMaps = []
+
+for map_file in map_files:
+    '''Iteratively going through the CSVs of the various maps on disk'''
+    individualMap = individualMapGenerator(map_file)
+    '''Appending each map to a list of maps'''
+    individualMaps.append(individualMap)
+    '''Presenting each map generated'''
+    mapPresenter(individualMap, map_file)
+
+'''We call the mapSticher() function inside the mapPresenter, to generate the stitched map and then present it'''
+mapPresenter(mapStitcher(individualMaps))
